6c1073683b573ca5034e3a6a7e65c5c8fbc233bf
Micaela Pujol <mpujoh00@estudiantes.unileon.es>
2022-01-29 19:38:59 +0100

    RB01_User-service Controllers' tests implementation

--- user-service/src/test/java/com/restaurant/booking/user/service/controller/AuthenticationControllerTest.java
+++ user-service/src/test/java/com/restaurant/booking/user/service/controller/AuthenticationControllerTest.java
@@ -1,22 +1,73 @@
 package com.restaurant.booking.user.service.controller;
 
+import com.restaurant.booking.user.model.*;
+import com.restaurant.booking.user.service.security.JwtUtils;
+import com.restaurant.booking.user.service.service.UserServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+
+import java.util.Set;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthenticationControllerTest {
 
     @Mock
-    private AuthenticationController authenticationController;
+    private UserServiceImpl userService;
+
+    @Mock
+    private AuthenticationManager authenticationManager;
+
+    @Mock
+    private JwtUtils jwtUtils;
+
+    @InjectMocks
+    private AuthenticationControllerImpl authenticationController;
 
     @Test
     void login(){
+        LoginRequest request = new LoginRequest("micaela@gmail.com", "1234");
+        UsernamePasswordAuthenticationToken userPassAuthToken =
+                new UsernamePasswordAuthenticationToken("micaela@gmail.com", "1234");
+        Authentication authentication = Mockito.mock(Authentication.class);
 
+        Mockito.when(authenticationManager.authenticate(userPassAuthToken)).thenReturn(authentication);
+        Mockito.when(jwtUtils.generateJwtToken(authentication)).thenReturn("JWT-TOKEN");
+
+        ResponseEntity<AuthenticationToken> responseEntity = authenticationController.login(request);
+
+        Mockito.verify(authenticationManager).authenticate(userPassAuthToken);
+        Mockito.verify(jwtUtils).generateJwtToken(authentication);
+
+        Assertions.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        Assertions.assertNotNull(responseEntity.getBody());
+        Assertions.assertEquals("JWT-TOKEN", responseEntity.getBody().getToken());
     }
 
-    @Test register(){
+    @Test
+    void register(){
+        User user = User.builder().email("micaela@gmail.com").password("1234")
+                .fullname("micaela").roles(Set.of(new Role(RoleName.ROLE_ADMIN))).build();
+        RegistrationRequest request = new RegistrationRequest
+                (user.getEmail(), user.getPassword(), user.getFullname(), RoleName.ROLE_ADMIN);
 
+        Mockito.when(userService.register(request)).thenReturn(user);
+
+        ResponseEntity<User> responseEntity = authenticationController.register(request);
+
+        Mockito.verify(userService).register(request);
+
+        Assertions.assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+        Assertions.assertNotNull(responseEntity.getBody());
+        Assertions.assertEquals(user, responseEntity.getBody());
     }
 }
--- user-service/src/test/java/com/restaurant/booking/user/service/controller/UserControllerTest.java
+++ user-service/src/test/java/com/restaurant/booking/user/service/controller/UserControllerTest.java
@@ -1,23 +1,48 @@
 package com.restaurant.booking.user.service.controller;
 
+import com.restaurant.booking.user.model.User;
+import com.restaurant.booking.user.service.service.UserServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
 public class UserControllerTest {
 
     @Mock
-    private UserController userController;
+    private UserServiceImpl userService;
+
+    @InjectMocks
+    private UserControllerImpl userController;
 
     @Test
     void getAllUsers(){
+        List<User> users = List.of(User.builder().email("micaela@gmail.com").build(),
+                User.builder().email("diego@gmail.com").build());
 
+        Mockito.when(userService.findAllUsers()).thenReturn(users);
+
+        List<User> obtainedUsers = userController.getAllUsers().getBody();
+
+        Mockito.verify(userService).findAllUsers();
+        Assertions.assertEquals(users, obtainedUsers);
     }
 
     @Test
     void getUserByEmail(){
+        User user = User.builder().email("micaela@gmail.com").build();
 
+        Mockito.when(userService.findByEmail("micaela@gmail.com")).thenReturn(user);
+
+        User obtainedUser = userController.getUserByEmail("micaela@gmail.com").getBody();
+
+        Mockito.verify(userService).findByEmail("micaela@gmail.com");
+        Assertions.assertEquals(user, obtainedUser);
     }
 }
