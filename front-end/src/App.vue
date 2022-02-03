bab8165a072d51f6158918223b977cec9eb5ed48
Micaela Pujol <mpujoh00@estudiantes.unileon.es>
2022-02-03 11:53:59 +0100

    RB01_User-service Login interface

---
+++ front-end/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-app>
+    <v-app-bar app color="pink lighten-3" dense elevation="3">
+      <!--icono-->
+      <v-app-bar-title style="margin-left: 10px; margin-right: 40px;">Restaurant Booking</v-app-bar-title>
+      <!-- secciones -->
+      <v-list class="d-flex align-center menu" style="background: transparent">
+        <v-list-item v-for="(menu, index) in menus" :key="index" :to="menu.route">
+          <v-list-item-title>{{menu.title}}</v-list-item-title>
+        </v-list-item>
+      </v-list>      
+      <v-spacer/>
+      <!-- login, usuario -->
+      <v-btn to="/login" plain>
+        <v-icon large>mdi-account-circle-outline</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      menus:[
+        {title: 'Home', route:'/'},
+        {title: 'About', route:'/about'}
+      ]
+    }
+  },
+}
+</script>
---
+++ front-end/src/views/Login.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-layout align-center justify-center>
+        <v-col cols="10" lg="4">
+            <v-card class="pa-4">
+                <div class="text-center pa-2">
+                    <v-avatar size="60" color="pink lighten-5">
+                        <v-icon size="40" color="pink lighten-2">mdi-account-circle</v-icon>
+                    </v-avatar>
+                    <h2 class="pa-1 pink--text text--lighten-2">Login</h2>
+                </div>
+                <v-form class="pt-2">
+                    <v-text-field label="Email" type="email" color="grey" prepend-inner-icon="mdi-at"/>
+                    <v-text-field label="Password" color="grey"
+                        :type="showPassword ? 'text': 'password'" 
+                        prepend-inner-icon="mdi-lock-outline" 
+                        :append-icon="showPassword ?  'mdi-eye-outline': 'mdi-eye-off-outline'"
+                        @click:append="showPassword = !showPassword"/>
+                </v-form>
+                <v-card-actions class="justify-center">
+                    <v-btn color="pink lighten-3">
+                        <span class="px-8">Login</span>
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-col>
+    </v-layout>
+    
+</template>
+
+<script>
+
+export default {
+    name: 'Login',
+
+    data() {
+        return {
+            showPassword: false
+        }
+    },
+}
+</script>
