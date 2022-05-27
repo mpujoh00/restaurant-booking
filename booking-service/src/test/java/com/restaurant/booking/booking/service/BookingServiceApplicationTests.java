59c19269474b54a005d4b39e7dcdd876b5c9561a
Micaela Pujol <mpujoh00@estudiantes.unileon.es>
2022-05-27 18:31:14 +0200

    RB04_Booking-service Added tests

--- booking-service/src/test/java/com/restaurant/booking/booking/service/BookingServiceApplicationTests.java
+++ booking-service/src/test/java/com/restaurant/booking/booking/service/BookingServiceApplicationTests.java
@@ -1,13 +1,25 @@
 package com.restaurant.booking.booking.service;
 
+import com.restaurant.booking.booking.service.controller.ReservationController;
+import com.restaurant.booking.booking.service.service.ReservationService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BookingServiceApplicationTests {
 
+	@Autowired
+	ReservationController reservationController;
+
+	@Autowired
+	ReservationService reservationService;
+
 	@Test
 	void contextLoads() {
+		Assertions.assertNotNull(reservationController);
+		Assertions.assertNotNull(reservationService);
 	}
 
 }
