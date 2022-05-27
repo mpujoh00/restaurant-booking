dd93f949542873a2db734fe8ba1e1976ad9f5e38
Micaela Pujol <mpujoh00@estudiantes.unileon.es>
2022-05-27 16:19:35 +0200

    RB04_Booking-service Fixed maven issues

--- booking-service/src/main/java/com/restaurant/booking/booking/service/controller/ReservationController.java
+++ booking-service/src/main/java/com/restaurant/booking/booking/service/controller/ReservationController.java
@@ -5,6 +5,7 @@ import com.restaurant.booking.booking.model.ReservationCreationRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
--- booking-service/src/main/java/com/restaurant/booking/booking/service/controller/ReservationSlotController.java
+++ booking-service/src/main/java/com/restaurant/booking/booking/service/controller/ReservationSlotController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
